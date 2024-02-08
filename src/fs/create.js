173c0c96124e9823daeb90c4b38422b00c6bf5bc
Lxvelly <aptem.otpaba@gmail.com>
2024-02-09 02:44:14 +0300

    feat: fs-create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import { existsSync, appendFile } from 'fs';
+
 const create = async () => {
-    // Write your code here 
+    if(existsSync('/home/tema/code/mitso-nodejs-basic/src/fs/files/fresh.txt')) {
+        console.log("FS eror")
+    } else {
+         appendFile('/home/tema/code/mitso-nodejs-basic/src/fs/files/fresh.txt', 'Im fresh and young', function (err) {
+        if(err) throw err;
+         console.log("Done")
+    });
+    }
+    
+   
 };
 
 await create();
