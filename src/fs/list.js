a08edbfd041d552780eaaaf41e8805e37be9092c
Lxvelly <aptem.otpaba@gmail.com>
2024-02-09 09:11:20 +0300

    feat: fs-list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,13 @@
+import {readdirSync} from "fs"
+
 const list = async () => {
-    // Write your code here 
+ console.log(readdirSync('/home/tema/code/mitso-nodejs-basic/src/fs/files', (err) => {
+    if(err) {
+        console.log('FS operation failed')
+    } else {
+        console.log('done')
+    }
+  }))
 };
 
 await list();
