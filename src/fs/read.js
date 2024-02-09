56c8204dd3e81b2f35911ae7aba87f226b78b344
Lxvelly <aptem.otpaba@gmail.com>
2024-02-09 09:18:40 +0300

    feat: fs-read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import {readFile} from "fs"
+
 const read = async () => {
-    // Write your code here 
+
+    readFile('/home/tema/code/mitso-nodejs-basic/src/fs/files/fileToRead.txt', (err, data) => {
+        if(err) {
+            console.log('FS operation failed')
+        } else {
+            console.log(Buffer.from(data,'utf-8').toString())
+        }
+    })
 };
 
 await read();
