7d35581d481a5988b4df4e98f0555e67818ccff2
Lxvelly <aptem.otpaba@gmail.com>
2024-02-09 09:01:09 +0300

    feat: fs-rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,11 @@
+import {existsSync, rename} from 'fs'
+
 const rename = async () => {
-    // Write your code here 
+    if(!existsSync('/home/tema/code/mitso-nodejs-basic/src/fs/files/wrongFilename.txt') || existsSync('/home/tema/code/mitso-nodejs-basic/src/fs/files/properFileName.md')) {
+        console.log('FS operation failed =(')
+    } else {
+        rename('/home/tema/code/mitso-nodejs-basic/src/fs/files/wrongFilename.txt', '/home/tema/code/mitso-nodejs-basic/src/fs/files/properFileName.md')
+    }
 };
 
 await rename();
