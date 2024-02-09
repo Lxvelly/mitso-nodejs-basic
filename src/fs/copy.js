2fb905f50c9aa9071a91f95d09b4ce0ac7b7fce8
Lxvelly <aptem.otpaba@gmail.com>
2024-02-09 03:14:19 +0300

    feat: fs copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,14 @@
+import { existsSync, cpSync } from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+    if(!existsSync('/home/tema/code/mitso-nodejs-basic/src/fs/files') || existsSync('/home/tema/code/mitso-nodejs-basic/src/fs/files-copy')) {
+        console.log("FS eror4ik")
+    } else {
+        cpSync('/home/tema/code/mitso-nodejs-basic/src/fs/files', '/home/tema/code/mitso-nodejs-basic/src/fs/files-copy', {
+            recursive: true,
+        });
+        console.log('done')
+    }
 };
 
 await copy();
