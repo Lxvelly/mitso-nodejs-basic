af637fae5cd65375c0ea8c07ad2d869fe16e70e0
Lxvelly <aptem.otpaba@gmail.com>
2024-02-09 09:36:27 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import {readFile} from 'fs'
+import {createHash} from 'crypto'
+
+function hash(string) {
+    return createHash('sha256').update(string).digest('hex')
+}
+
 const calculateHash = async () => {
-    // Write your code here 
+    readFile('/home/tema/code/mitso-nodejs-basic/src/hash/files/fileToCalculateHashFor.txt', (err,data) => {
+    if(err) {
+        console.log('FS operation failed')
+    } else {
+        console.log(hash(data))
+    }
+   })
 };
 
 await calculateHash();
